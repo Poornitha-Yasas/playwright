f9e8a43bfeb59e679f1992b4bd93d6f9b4419474
Poornitha-Yasas <poornithayasasx0@gmail.com>
2024-12-24 21:08:32 +0530

    Hooks and Flow Control

--- tests/firstTest.spec.ts
+++ tests/firstTest.spec.ts
@@ -1,7 +1,36 @@
 import {test} from '@playwright/test'
 
-test('the first test', async ({page}) => {
+
+test.beforeEach(async ({page}) => {
     await page.goto('http://localhost:4200/')
-    await page.getByText('Forms').click()
-    await page.getByText('Form Layouts').click()
+})
+
+
+test.describe('suite1', () => {
+    test.beforeEach(async ({page}) => {
+        await page.getByText('Extra Components').click()
+    })
+
+    test('the first test', async ({page}) => {
+        await page.getByText('Calendar').click()
+    })
+
+})
+
+
+test.describe('suite2', () => {
+    test.beforeEach(async ({page}) => {
+        await page.getByText('Forms').click()
+    })
+
+    test('the first test1', async ({page}) => {
+    
+        await page.getByText('Form Layouts').click()
+    })
+    
+    test('navigate to datepicker page1', async ({page}) => {
+        
+        await page.getByText('Datepicker').click()
+    })
+
 })
