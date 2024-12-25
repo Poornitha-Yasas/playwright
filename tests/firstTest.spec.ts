0f2d3520ff8c8aa0debe42a298d335d3fa543cf2
Poornitha-Yasas <poornithayasasx0@gmail.com>
2024-12-25 20:57:39 +0530

    Locator Systax

--- tests/firstTest.spec.ts
+++ tests/firstTest.spec.ts
@@ -3,34 +3,37 @@ import {test} from '@playwright/test'
 
 test.beforeEach(async ({page}) => {
     await page.goto('http://localhost:4200/')
+    await page.getByText('Forms').click()
+    await page.getByText('Form Layouts').click()
 })
 
 
-test.describe('suite1', () => {
-    test.beforeEach(async ({page}) => {
-        await page.getByText('Extra Components').click()
-    })
+test('locator systax rules', async({page}) => {
+    //By Tag Name
+    await page.locator('input').first().click()
 
-    test('the first test', async ({page}) => {
-        await page.getByText('Calendar').click()
-    })
+    //By ID
+    page.locator('inputEmail1')
 
-})
+    //By class value
+    page.locator('.shape-rectangle')
 
+    //By attribiute
+    page.locator('[placeholder="Email"]')
 
-test.describe('suite2', () => {
-    test.beforeEach(async ({page}) => {
-        await page.getByText('Forms').click()
-    })
+    //By class value full
+    page.locator('[class="input-full-width size-medium status-basic shape-rectangle nb-transition"]')
 
-    test('the first test1', async ({page}) => {
-    
-        await page.getByText('Form Layouts').click()
-    })
-    
-    test('navigate to datepicker page1', async ({page}) => {
-        
-        await page.getByText('Datepicker').click()
-    })
+    //Combine different selectors
+    page.locator('input[placeholder="Email"]')
+
+    //By Xpath (Not Recommended)
+    page.locator('//*[@id="inputEmail1"]')
+
+    //By partial text match
+    page.locator(':text("Using')
+
+    //By exact text match
+    page.locator(':text-is("Using the Grid")')
 
 })
