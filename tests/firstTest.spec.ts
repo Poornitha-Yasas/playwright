8c5c71b383f814dfc284aa1d2b3041a3c4da07b1
Poornitha-Yasas <poornithayasasx0@gmail.com>
2024-12-26 12:13:00 +0530

    User Facing Locators

--- tests/firstTest.spec.ts
+++ tests/firstTest.spec.ts
@@ -37,3 +37,18 @@ test('locator systax rules', async({page}) => {
     page.locator(':text-is("Using the Grid")')
 
 })
+
+test('User facing locators', async({page}) => {
+    await page.getByRole('textbox',{name: "Email"}).first().click()
+    await page.getByRole('button',{name:"Sign in"}).first().click()
+
+    await page.getByLabel('Email').first().click()
+
+    await page.getByPlaceholder('Jane Doe').click()
+
+    await page.getByText('Using the Grid').click()
+
+    await page.getByTestId('SignIn').click()
+
+    await page.getByTitle('IoT Dashboard').click()
+})
