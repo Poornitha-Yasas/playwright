b6192120f558c62ba403fcd5923ab0150f4d9846
Poornitha-Yasas <poornithayasasx0@gmail.com>
2025-01-16 19:45:20 +0530

    last

--- tests/usePageObjects.spec.ts
+++ tests/usePageObjects.spec.ts
@@ -1,5 +1,7 @@
 import {test, expect} from '@playwright/test'
 import {NavigationPage} from '../page-objects/navigationPage'
+import { FormLayoutsPage } from '../page-objects/formLayoutsPage'
+import { DatepickerPage } from '../page-objects/datepickerPage'
 
 test.beforeEach(async({page}) => {
   await page.goto('http://localhost:4200/')
@@ -10,6 +12,19 @@ test('navigate to form page', async({page}) => {
   await navigateTo.formLayoutsPage()
   await navigateTo.datepickerPage()
   await navigateTo.smartTablePage()
-  await navigateTo.toastrPage()
+  await navigateTo.toastPage()
   await navigateTo.tooltipPage()
+})
+
+test('parametrized methods', async({page}) => {
+  const navigateTo = new NavigationPage(page)
+  const onFormLayoutsPage = new FormLayoutsPage(page)
+  const onDatepickerPage = new DatepickerPage(page)
+
+  await navigateTo.formLayoutsPage()
+  await onFormLayoutsPage.submitUsingTheGridFormWithCredentialsAndSelectOption('tesdt@test.com', 'Welcome1', 'Option 2')
+  await onFormLayoutsPage.sumbitInlineFormWithNameEmailAndCheckbox('John Smith', 'John@test.com', false)
+  await navigateTo.datepickerPage()
+  await onDatepickerPage.selectCommonDatePickerDateFromToday(10)
+  await onDatepickerPage.selectDatepickerWithRangeFromToday(6, 15)
 })
